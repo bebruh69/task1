e6b7884448fc3298f5aa92cdcd9a82c0a010b4e1
glofst <forglofst@gmail.com>
2021-09-15 15:27:56 +0300

    Output update

--- main.cpp
+++ main.cpp
@@ -5,17 +5,12 @@ using namespace std;
 int main()
 {
     ofstream out ("/home/batman/school/task1/result.txt") ;
-    string question, answer, getcontent;
-    ifstream in ("/home/batman/school/task1/questions.txt");
-    if(in.is_open())
-    {
-        while(getline(in, question))
-        {
-            cout << question << endl;
-            cin >> answer;
-            out << answer << endl;
-        }
-    }
+    string name, favourite, getcontent;
+    cout << "ФИО: ";
+    cin >> name;
+    cout << "Рекомендуемая книга: ";
+    cin >> favourite;
+    out << name << endl << favourite;
     out.close();
     ifstream openfile ("/home/batman/school/task1/result.txt");
     if(openfile.is_open())
