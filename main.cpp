b2e9766537847a70ad8c37afa812d6390f3110a3
glofst <forglofst@gmail.com>
2021-09-15 10:11:35 +0300

    First Commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+    ofstream out ("/home/batman/school/task1/result.txt") ;
+    string name, favourite;
+    cout << "ФИО: ";
+    cin >> name;
+    cout << "Любимый фильм: ";
+    cin >> favourite;
+    out << name << endl << favourite;
+    out.close();
+    return 0;
+}
