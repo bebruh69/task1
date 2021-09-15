b50528c986867beb4da3c0b14494122223587e3a
glofst <forglofst@gmail.com>
2021-09-15 10:32:10 +0300

    Revert "Added new question"
    This reverts commit b645851effc48f2b19e43627e3a041c55fae9b03.

--- main.cpp
+++ main.cpp
@@ -5,14 +5,12 @@ using namespace std;
 int main()
 {
     ofstream out ("/home/batman/school/task1/result.txt") ;
-    string name, favourite, kprf;
+    string name, favourite;
     cout << "ФИО: ";
     cin >> name;
     cout << "Рекомендуемая книга: ";
     cin >> favourite;
-    cout << "За какую партию планируете голосовать: ";
-    cin >> kprf;
-    out << name << endl << favourite << endl << kprf;
+    out << name << endl << favourite;
     out.close();
     return 0;
 }
