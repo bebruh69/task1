9f47089ae859a48eaf735b3261871e9c2efbd0cc
glofst <forglofst@gmail.com>
2021-09-17 21:57:08 +0300

    Added choice menu

--- main.cpp
+++ main.cpp
@@ -4,21 +4,35 @@
 using namespace std;
 int main()
 {
-    ofstream out ("/home/batman/school/task1/result.txt") ;
-    string name, favourite, getcontent;
-    cout << "ФИО: ";
-    cin >> name;
-    cout << "Рекомендуемая книга: ";
-    cin >> favourite;
-    out << name << endl << favourite;
-    out.close();
-    ifstream openfile ("/home/batman/school/task1/result.txt");
-    if(openfile.is_open())
+    ofstream out ("/home/batman/school/task1/result.txt", ios_base::app) ;
+    ifstream res ("/home/batman/school/task1/result.txt");
+    string question, answer, result;
+    int choice;
+    cout << "Отправьте:" << endl << "1, если Вам нужно провести опрос пользователя" << endl << "2, если Вам нужны результаты опроса" << endl;
+    cin >> choice;
+    ifstream in ("/home/batman/school/task1/questions.txt");
+    if(choice == 1)
     {
-        while(getline(openfile, getcontent))
+        if(in.is_open())
         {
-            cout << getcontent << endl;
+            while(getline(in, question))
+            {
+                cout << question << endl;
+                cin >> answer;
+                out << answer << endl;
+            }
         }
     }
+    if(choice == 2)
+    {
+        if(res.is_open())
+        {
+            while(getline(res, result))
+            {
+                cout << result << endl;
+            }
+        }
+    }
+    out.close();
     return 0;
 }
