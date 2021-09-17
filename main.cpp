f5f736c280f05762e93a36a0a4b84ec4efe6d53e
glofst <forglofst@gmail.com>
2021-09-17 21:17:45 +0300

    Input update

--- main.cpp
+++ main.cpp
@@ -5,12 +5,17 @@ using namespace std;
 int main()
 {
     ofstream out ("/home/batman/school/task1/result.txt") ;
-    string name, favourite;
-    cout << "ФИО: ";
-    cin >> name;
-    cout << "Рекомендуемая книга: ";
-    cin >> favourite;
-    out << name << endl << favourite;
+    string question, answer;
+    ifstream in ("/home/batman/school/task1/questions.txt");
+    if(in.is_open())
+    {
+        while(getline(in, question))
+        {
+            cout << question << endl;
+            cin >> answer;
+            out << answer << endl;
+        }
+    }
     out.close();
     return 0;
 }
